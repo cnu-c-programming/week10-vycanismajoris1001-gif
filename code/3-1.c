8cd6c8ee71228810d53af17e9d981b0cf94617e6
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-05-08 10:19:29 +0900

    Update 3-1.c

---
+++ code/3-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  int *space = malloc(10*sizeof(int));
+  struct Point *next = &a;
+  struct Point a = {a.name, a.score, &b};
+  struct Point b = {b.name, b.score, &c};
+  struct Point c = {c.name, c.score, NULL};
+
+  for (struct Point *p =
+
+  scanf("%s %s %s",  ,&name, &score);
+add, delete, print
+  free(space);
+}
+
+struct Point {
+  string name;
+  int score;
+  struct Point *next;
+
+  
+};
